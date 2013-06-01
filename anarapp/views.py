7f9d838c8dee7c1a2568634084e393d110f6b6ba
ML <emele18@gmail.com>
2013-06-01 15:43:42 -0430

    agrega template de resultados + estilos

--- anarapp/views.py
+++ anarapp/views.py
@@ -1,8 +1,9 @@
 #coding: latin-1
 
-from anarapp.models import Yacimiento
+from anarapp.models import Yacimiento, Piedra
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader, Context
 
 # Create your views here.
 
@@ -10,4 +11,8 @@ def index(request):
     return HttpResponse("Software Interactivo ANAR.")
 
 def results(request):
-	return HttpResponse("Estas viendo los resultados de la busqueda.")
+	template = loader.get_template('anarapp/results.html')
+	lista_yacimientos = Yacimiento.objects.all().order_by('codigo')
+	lista_piedras = Piedra.objects.all().order_by('codigo')
+	context = Context({"lista_yacimientos": lista_yacimientos, "lista_piedras": lista_piedras})
+	return HttpResponse(template.render(context))
