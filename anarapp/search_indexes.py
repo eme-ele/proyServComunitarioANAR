dacdba6cd80bbe80579ce014bedc18a4f655ced5
Linda <tefa@note-ubuntu1204.(none)>
2013-06-26 18:45:46 -0430

    Arreglo de Diego al modelo

--- anarapp/search_indexes.py
+++ anarapp/search_indexes.py
@@ -1,21 +1,18 @@
 import datetime
 from haystack import indexes
-from anarapp.models import Yacimiento, Piedra
+from haystack.fields import MultiValueField
+from anarapp.models import Yacimiento, ManifestUbicacionYacimiento
 
 class YacimientoIndex(indexes.SearchIndex, indexes.Indexable):
 	text = indexes.CharField(document=True, use_template=True)
+	manifestaciones = MultiValueField()
 
 	def get_model(self):
 		return Yacimiento
-
-	def index_queryset(self, using=None):
-		return self.get_model().objects.all()
-
-class PiedraIndex(indexes.SearchIndex, indexes.Indexable):
-	text = indexes.CharField(document=True, use_template=True)
-
-	def get_model(self):
-		return Piedra
+		
+	def prepare_manifestacion(self, obj):
+		lista = ManifestUbicacionYacimiento.objects.filter(yacimiento = obj_id)
+		return [m.tipoManifestacion for m in lista.all()]
 
 	def index_queryset(self, using=None):
 		return self.get_model().objects.all()
